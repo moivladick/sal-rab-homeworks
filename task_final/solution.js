ac6c7874feda727be69e7c06a796ef08830b9c66
moivladick <vlavladimail02@gmail.com>
2022-01-06 20:45:51 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,18 +36,23 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
 
+    data.client = name + " " + phone;
+
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i <= countOfGoods; i++) {
+        if (goods[i]) {
+            data.goods.push({
+                title: goods[i].title,
+                count: goods[i].count
+            });
+        }
     }
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`    
+    // data.order.address = address;
+    data.order.sum = sum;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
